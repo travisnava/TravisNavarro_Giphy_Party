99dbabde27a351f2d4106f08c0695f40dfe22c61
Travis Navarro <65472356+travisnava@users.noreply.github.com>
2022-06-08 19:31:45 -0400

    finished most core features, unable to get load more functionality

---
+++ script.js
@@ -0,0 +1,64 @@
+
+
+// global constants
+//const apiKey = AIzaSyDgKyDy5h8nVBo0NSDLky9qj0SXSWrz_eQ        (MY TENOR API KEY)
+
+
+const apiKey = "z9qEJKCAcrrMCA7q77qHJXLDkv111Kra"  // TA'S API KEY
+const rating = "G"
+const limit = "9"
+
+// grabbing elements
+let giphyForm = document.querySelector("#form");
+let searchBtn = document.querySelector("#search-btn");
+let loadBtn = document.querySelector("#load-btn");
+let gifFeed = document.querySelector("#gif-feed");
+
+
+
+giphyForm.addEventListener("submit", (evt) => {
+    // prevent page from reload
+    evt.preventDefault();
+    console.log("evt.target.gif.value =", evt.target.gif.value);
+    let apiUrl = "http://api.giphy.com/v1/gifs/search" + "?api_key=" + apiKey +  "&limit=" + limit + "&rating=" + rating +"&q=" + evt.target.gif.value;
+    console.log("apiUrl =", apiUrl);
+    gifFeed.innerHTML = ""
+    console.log(getResults(apiUrl));
+    form.reset();
+    loadBtn.classList.remove('hidden');
+    
+
+    
+
+})
+
+
+async function getResults(apiUrl) {
+    let response =  await fetch(apiUrl);
+    console.log("response is: ", response);
+    let responseData =  await response.json()
+    displayResults(responseData);
+
+}
+
+function displayResults(gifData) {
+    gifData.data.forEach((gif) => {
+        gifFeed.innerHTML += `<img src="${gif.images.fixed_height.url}">`
+    });
+
+
+}
+
+ 
+
+
+
+    
+
+
+
+
+    //gifData.data.forEach((gif) => {
+    //    gifFeed.innerHTML += `<img src="${gif.images.original.url}">`
+    //});
+
